651860507ae1111d397aa9168caf311bcff50226
Chen XM <oscar999@mail.ustc.edu.cn>
2021-03-31 21:52:16 +0800

    Java IO

--- jcodef-func/src/main/java/cn/osxm/jcodef/func/io/FileIoNioDemo.java
+++ jcodef-func/src/main/java/cn/osxm/jcodef/func/io/FileIoNioDemo.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,11 +28,13 @@ public class FileIoNioDemo {
 
 	private String sourceFullFileName = null;
 	private String targetFullFileName = null;
+	private String fullFileName = null;
 	
 	@Before
 	public void setup() {
 		String sourceFileName = "1.rar";
 		String targetFileName = "1_1.rar";
+		String fileName = "1.txt";
 		File file = new File("");
 		String projectPath = "";
 		try {
@@ -43,6 +46,7 @@ public class FileIoNioDemo {
 		
 		sourceFullFileName = projectPath + File.separator +"files"+File.separator+sourceFileName;
 		targetFullFileName = projectPath + File.separator+"files"+File.separator+targetFileName;
+		fullFileName = projectPath + File.separator+"files"+File.separator+fileName;
 		File targetFile = new File(targetFullFileName);
 		if(targetFile.exists()) {
 			targetFile.delete();
@@ -52,6 +56,26 @@ public class FileIoNioDemo {
 	
 	
 	@Test
+	public void fileNio() throws Exception{
+			FileInputStream fileInputStream = new FileInputStream(new File(fullFileName));
+            FileChannel fileChannel = fileInputStream.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            while(fileChannel.read(byteBuffer)>0){
+                //将ByteBuffer从写模式转换为读取模式
+            	// 实现方式是把position的位置变成0，limit的位置变成position。
+                byteBuffer.flip();
+                //打印缓冲区内容
+                Charset charset = Charset.forName("UTF-8");
+                System.out.println(charset.newDecoder().decode(byteBuffer).toString());
+                //这里并不是清除缓冲区的数据，而是指针的位移。把position为0，把limit为缓冲区的容量大小
+                byteBuffer.clear();
+            }
+            fileChannel.close();
+            fileInputStream.close();
+	}
+	
+	
+	//@Test
 	public void writeFileUseIo() throws Exception{
 		long lBegin = System.currentTimeMillis();
 		FileInputStream fileInputStream = new FileInputStream(new File(sourceFullFileName));
@@ -68,21 +92,21 @@ public class FileIoNioDemo {
 	    System.out.println("传统IO花费时间："+(lEnd-lBegin)+"毫秒");
 	}
 	
-	@Test
+	//@Test
 	public void writeFileUseNio() throws Exception{
 		long lBegin = System.currentTimeMillis();
 		FileInputStream fileInputStream = new FileInputStream(new File(sourceFullFileName));
-		FileChannel inChannel = fileInputStream.getChannel();
+		FileChannel inChannel = fileInputStream.getChannel();//获取输入通道
 		FileOutputStream fileOutputStream = new FileOutputStream(new File(targetFullFileName));
-		FileChannel outChannel = fileOutputStream.getChannel();
-		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);  
+		FileChannel outChannel = fileOutputStream.getChannel();//获取输出通道
+		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);  //字节缓冲区
 		while(true) {
-			int eof = inChannel.read(byteBuffer);
+			int eof = inChannel.read(byteBuffer); //从通道读取数据到字节数组
 			if (eof == -1)
 				break;
-			byteBuffer.flip();
-			outChannel.write(byteBuffer);
-			byteBuffer.clear();
+			byteBuffer.flip(); //将postion复位到0
+			outChannel.write(byteBuffer); //写入通道
+			byteBuffer.clear(); //
 		}
 		inChannel.close();
 		fileInputStream.close();
--- jcodef-func/src/main/java/cn/osxm/jcodef/func/io/FileIoType.java
+++ jcodef-func/src/main/java/cn/osxm/jcodef/func/io/FileIoType.java
@@ -8,6 +8,8 @@
  */
 package cn.osxm.jcodef.func.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -23,16 +25,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
-  * @ClassName: FileIoType
-  * @Description: TODO
-  * @author oscarchen
-  */
+ * @ClassName: FileIoType
+ * @Description: TODO
+ * @author oscarchen
+ */
 public class FileIoType {
-	
-	private static String inFileFullName;
-	private static String outFileFullName;
-	
-	
+
+	private static String fullFileName;
+	private static String inFullFileName;
+	private static String outFullFileName;
+
 	/**
 	 * JUnit 测试初始化方法， @BeforeClass 多个测试方法只会执行一次
 	 */
@@ -43,26 +45,32 @@ public class FileIoType {
 		File file = new File("");
 		String projectPath = "";
 		try {
-			projectPath = file.getCanonicalPath();  //项目根路径
+			projectPath = file.getCanonicalPath(); // 项目根路径
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		inFileFullName = projectPath + File.separator +"files"+File.separator+inFileName;
-		outFileFullName = projectPath + File.separator+"files"+File.separator+outFileName;
+		inFullFileName = projectPath + File.separator + "files" + File.separator + inFileName;
+		fullFileName = inFullFileName;
+		outFullFileName = projectPath + File.separator + "files" + File.separator + outFileName;
 	}
-	
-	
-	
+
 	/**
 	 * JUnit 测试初始化方法， @Before 每个测试方法之前都只会执行
-	 */	
+	 */
 	@Before
-	public void setup() {
-		File outFile = new File(outFileFullName);
-		//if(outFile.exists()) {
-		//	outFile.delete();
-		//}
+	public void setup() throws IOException {
+		File outFile = new File(outFullFileName);
+		// if(outFile.exists()) {
+		// outFile.delete();
+		// }
+		File file = new File(fullFileName);
+		if (file.exists()) { //清空文件内容
+			FileWriter fileWriter = new FileWriter(file);
+			fileWriter.write("");
+			fileWriter.flush();
+			fileWriter.close();
+		}
 	}
 
 	/**
@@ -70,40 +78,72 @@ public class FileIoType {
 	 */
 	@Test
 	public void byteStreamDemo() throws IOException {
-	
-		//1.输出流
-		File outFile = new File(outFileFullName);
-		OutputStream out = new FileOutputStream(outFile); //文件输出流
+
+		// 1.输出流
+		File outFile = new File(outFullFileName);
+		OutputStream out = new FileOutputStream(outFile); // 文件输出流
 		out.write("输出流测试".getBytes());
 		out.flush();
 		out.close();
-		
-		//2. 输入流
-		File inFile = new File(inFileFullName);
-		InputStream in = new FileInputStream(inFile); //文件输入流
-		byte[] b = new byte[1024];//定义字节数组
-		int len = in.read(b); //从字节流中读取字节数据到字节数组， len是读取的长度， 如果该长度等于-1，即读取完成
-		String str = new String(b,0,len);
+
+		// 2. 输入流
+		File inFile = new File(inFullFileName);
+		InputStream in = new FileInputStream(inFile); // 文件输入流
+		byte[] b = new byte[1024];// 定义字节数组
+		int len = in.read(b); // 从字节流中读取字节数据到字节数组， len是读取的长度， 如果该长度等于-1，即读取完成
+		String str = new String(b, 0, len);
 		System.out.println(str);
-		in.close(); //关闭输入流		
-		
-		
+		in.close(); // 关闭输入流
+
 	}
-	
+
+	/**
+	 * 字符流
+	 * 
+	 * @throws IOException
+	 */
 	@Test
-	public void charStreamDemo() throws IOException{	
+	public void charStreamDemo() throws IOException {
 		String str = "Hello,Java IO";
-		//File file = new File(inFileFullName); //1.txt
-		Writer out = new FileWriter(inFileFullName);
+		// File file = new File(inFullFileName); //1.txt
+		Writer out = new FileWriter(inFullFileName);
 		out.write(str);
 		out.flush();
 		out.close();
-		
-		FileReader fr = new FileReader(inFileFullName);
+
+		FileReader fr = new FileReader(inFullFileName);
 		int ichar = 0;
-		while((ichar=fr.read())!=-1) {
-			System.out.print((char)ichar);
+		while ((ichar = fr.read()) != -1) {
+			System.out.print((char) ichar);
 		}
-		
+
+	}
+
+	/**
+	 * 传统IO 的缓冲流
+	 */
+	@Test
+	public void buffStreamDemo() throws IOException {
+		File file = new File(fullFileName);
+		// 1. 字节流缓冲
+		// 1.1 字节输出流缓冲
+		OutputStream out = new FileOutputStream(file);
+		BufferedOutputStream bufOut = new BufferedOutputStream(out);
+		bufOut.write("Hello,Buffer byte Stream".getBytes());
+		bufOut.flush();
+		bufOut.close();
+		out.close();
+		// 1.21 字节输入流缓冲
+		InputStream in = new FileInputStream(file);
+		BufferedInputStream bufIn = new BufferedInputStream(in);
+		byte[] b = new byte[1024];
+		bufIn.read();
+		int len = 0;
+		while ((len = bufIn.read(b, 0, len)) != -1) { // 如果读取没有完成
+			String str = new String(b, 0, len);
+			System.out.println(str);
+		}
+		bufIn.close();
+		in.close();
 	}
 }
